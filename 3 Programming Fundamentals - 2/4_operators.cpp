b2398f8e28bfc540072d3ea8932d6d172c3a5641
Chaudhary Sarimurrab <35756749+sarimurrab@users.noreply.github.com>
2020-03-24 23:59:44 +0530

    Add files via upload

---
+++ 3 Programming Fundamentals - 2/4_operators.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // SHIFT OPERATOR
+    int x = 5, y = 10;
+    x = x << 3; // x * pow (2,3)
+    cout << x;
+    cout << endl;
+    y = y >> 2; // y / pow(2,2)
+    cout << y;
+
+    
+
+
+
+}
